ebcf1ead5be2b928d046bc6baeccad79e0023af3
Isaac Liljeros <isaak.liljeros@gmail.com>
2021-06-01 20:09:18 -0400

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,28 @@
+package org.example;
+
+import java.util.Scanner;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main( String[] args ) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Variables
+        double euros, exchangeRate, dollars;
+
+        System.out.print("How many Euros are you exchanging? ");
+        euros = scanner.nextDouble();
+        System.out.print("What is the exchange rate? ");
+        exchangeRate = scanner.nextDouble();
+
+        dollars = euros * exchangeRate / 100;
+
+        System.out.println(euros + " euros at an exchange rate of " + exchangeRate + " is");
+        System.out.println(String.format("%.2f", dollars) + " U.S. dollars.");
+
+
+
+    }
+}
